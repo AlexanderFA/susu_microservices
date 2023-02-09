73230930106d7c1e50be15e0c523abc4a7e70570
Aleksandr Zontov <Aleksandr.Zontov@altenar.com>
2023-02-10 02:04:38 +0300

    Lab1 - 3

--- lab1/client.py
+++ lab1/client.py
@@ -1,16 +1,29 @@
 import socket
+import os
 
 sock = socket.socket()
 sock.connect(('localhost', 9090))
 
-user_name = input("Enter your name: ")
-sock.send(user_name.encode())
+SID = os.getenv('SID') or '-'  # эмуляция сессионного токена (берем из переменной окружения)
+sock.send(SID.encode())  # отправляем сессионный токен, чтобы сервер нас узнал
+
+# user_name = input("Enter your name: ")
+# sock.send(user_name.encode())
+# print("Server response: {}".format(sock.recv(1024).decode("utf-8")))
+
+server_msg = sock.recv(1024).decode("utf-8")
+if server_msg == 'Enter your name: ':
+    user_name = input(server_msg)
+    sock.send(bytes(user_name, "utf-8"))
+    print(sock.recv(1024).decode("utf-8"))
+else:
+    print(server_msg)  # welcome back
 
 while True:
-    msg = input("Type a message (type '\\exit' to exit): ")
+    msg = input()
+    sock.send(bytes(msg, "utf-8"))  # отправляем сообщение даже если это \exit
+    print("Server response: {}".format(sock.recv(1024).decode("utf-8")))
     if msg == "\\exit":
         break
-    sock.send(msg.encode())
-    print("Server response: {}".format(sock.recv(1024).decode("utf-8")))
 
 sock.close()
--- lab1/server.py
+++ lab1/server.py
@@ -1,18 +1,44 @@
 import socket
+import threading
+from datetime import datetime
+
+clients = {}
 
 sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(2)
 print('waiting for connection...')
-conn, addr = sock.accept()
-print('connected: ', addr)
 
-user_name = conn.recv(1024).decode("utf-8")
-print('user_name = ' + user_name)
+
+def handle_client(conn, addr):
+    # conn.send(bytes(f"Welcome back {clients[client_socket]}!", "utf-8"))
+    SID = conn.recv(1024).decode("utf-8")
+    print(f"User with sid {SID} entered")
+    if SID in clients:
+        print(clients[SID])
+        user_name = clients[SID]['name']
+        now_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        conn.send(bytes(f"Welcome back {user_name}! Time now is: {now_time}", "utf-8"))
+    else:
+        conn.send(bytes("Enter your name: ", "utf-8"))
+        user_name = conn.recv(1024).decode("utf-8")
+        clients[SID] = {'name': user_name}
+        conn.send(bytes(f"Hello {user_name}! Start to send messages!", "utf-8"))
+
+    while True:
+        client_msg = conn.recv(1024).decode("utf-8")
+        if client_msg == "\\exit":
+            conn.close()
+            break
+        client_msg = bytes("User {} wrote: {}".format(user_name, client_msg), "utf-8")
+        conn.send(client_msg)
+
 
 while True:
-    client_msg = conn.recv(1024).decode("utf-8")
-    client_msg = bytes("User {} wrote: {}".format(user_name, client_msg), "utf-8")
-    conn.send(client_msg)
+    conn, addr = sock.accept()  # conn это и есть сокет
+    print(conn)
+    print(f"{addr} connected")
+    client_thread = threading.Thread(target=handle_client, args=(conn, addr))
+    client_thread.start()
 
 conn.close()
