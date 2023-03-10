8db565dc628fc8d5fb5668e0cf38c55f3d942576
Aleksandr Zontov <Aleksandr.Zontov@altenar.com>
2023-03-10 19:52:30 +0300

    Lab1 - 5 - Users now can chat with each other

--- lab1/client.py
+++ lab1/client.py
@@ -1,5 +1,7 @@
 import socket
 import os
+import threading
+import time
 
 sock = socket.socket()
 sock.connect(('localhost', 9090))
@@ -19,11 +21,31 @@ if server_msg == 'Enter your name: ':
 else:
     print(server_msg)  # welcome back
 
+# while True:
+#     msg = input()
+#     sock.send(bytes(msg, "utf-8"))  # отправляем сообщение даже если это \exit
+#     # print("Server response: {}".format(sock.recv(1024).decode("utf-8")))
+#     print(sock.recv(1024).decode("utf-8"))
+#     if msg == "\\exit":
+#         break
+keep_running = True
+
+
+def listen_server():
+    global keep_running
+    while keep_running:
+        print(sock.recv(1024).decode("utf-8"))
+
+
+listen_server_thread = threading.Thread(target=listen_server, args=())
+listen_server_thread.start()
+
 while True:
     msg = input()
     sock.send(bytes(msg, "utf-8"))  # отправляем сообщение даже если это \exit
-    print("Server response: {}".format(sock.recv(1024).decode("utf-8")))
     if msg == "\\exit":
         break
 
+keep_running = False
+listen_server_thread.join()
 sock.close()
--- lab1/server.py
+++ lab1/server.py
@@ -10,29 +10,47 @@ sock.listen(2)
 print('waiting for connection...')
 
 
+def send_to_all(msg):
+    for client_SID, client_data in clients.items():
+        if client_data['conn'] is not None:
+            client_data['conn'].send(msg)
+
+
+# отправка сообщения всем клиентам, кроме отправителя
+def sent_to_all_except(msg, current_sid):
+    for client_SID, client_data in clients.items():
+        if client_SID != current_sid and client_data['conn'] is not None:
+            client_data['conn'].send(msg)
+
+
 def handle_client(conn, addr):
-    # conn.send(bytes(f"Welcome back {clients[client_socket]}!", "utf-8"))
     SID = conn.recv(1024).decode("utf-8")
-    print(f"User with sid {SID} entered")
+    # print(f"User with sid {SID} entered")
+    now_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     if SID in clients:
-        print(clients[SID])
         user_name = clients[SID]['name']
-        now_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        clients[SID]['conn'] = conn
         conn.send(bytes(f"Welcome back {user_name}! Time now is: {now_time}", "utf-8"))
     else:
         conn.send(bytes("Enter your name: ", "utf-8"))
         user_name = conn.recv(1024).decode("utf-8")
-        clients[SID] = {'name': user_name}
+        clients[SID] = {
+            'name': user_name,
+            'conn': conn,
+        }
         conn.send(bytes(f"Hello {user_name}! Start to send messages!", "utf-8"))
+
+    sent_to_all_except(bytes(f"User {user_name} entered the room {now_time}", "utf-8"), SID)
 
     while True:
         client_msg = conn.recv(1024).decode("utf-8")
         if client_msg == "\\exit":
-            conn.send(bytes(f"{user_name} left chat!", "utf-8"))
+            clients[SID]['conn'] = None
             conn.close()
+            send_to_all(bytes(f"{user_name} has left the chat!", "utf-8"))
             break
-        client_msg = bytes("User {} wrote: {}".format(user_name, client_msg), "utf-8")
-        conn.send(client_msg)
+
+        send_to_all(bytes("\033[1;32m{}\033[0m: {}".format(user_name, client_msg), "utf-8"))
 
 
 while True:
