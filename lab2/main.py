8872b4a1e2cb296a27ba0ce8a4f22faf412f035f
Aleksandr Zontov <Aleksandr.Zontov@altenar.com>
2023-03-15 22:57:44 +0300

    Create test data

---
+++ lab2/main.py
@@ -0,0 +1,10 @@
+data = {
+    'words': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+             'Mauris adipiscing adipiscing placerat. Vestibulum augue augue, '
+             'pellentesque quis sollicitudin id, adipiscing.',
+    'tuple': tuple(range(100)),
+    'dict': dict((str(i), 'a') for i in range(100)), 'int': 100,
+    'float': 100.123456
+}
+
+print(data)
